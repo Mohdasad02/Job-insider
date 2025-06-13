e69a9131ecb9ede0001d40f816a422b436d8e367
Mohdasad02 <138427107+Mohdasad02@users.noreply.github.com>
2025-06-13 23:37:16 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(cors())
 app.use(express.json())
 
 //routes
-app.get('/',(req,res) => res.send("API working"))
+app.get('/api',(req,res) => res.send("API working"))
 app.get("/debug-sentry", function mainHandler(req, res) {
   throw new Error("My first Sentry error!");
 });
@@ -28,6 +28,7 @@ app.post('/webhooks',clerkWebhooks)
 const PORT = process.env.PORT || 5000
 Sentry.setupExpressErrorHandler(app);
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
-})
+// app.listen(PORT,()=>{
+//     console.log(`Server is running on port ${PORT}`);
+// })
+export default app;
